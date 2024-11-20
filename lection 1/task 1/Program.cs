04da0a595e92f60dc3849c872dc3041c1ae09590
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-20 14:13:18 +0200

    document formate

--- lection 1/task 1/Program.cs
+++ lection 1/task 1/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Globalization;
 
 Console.WriteLine("number 1: ");
-string StrNumberOne =Console.ReadLine();
+string StrNumberOne = Console.ReadLine();
 
 Console.WriteLine("number 2: ");
 string StrNumberTwo = Console.ReadLine();
@@ -11,4 +11,4 @@ int IntNumberTwo = int.Parse(StrNumberTwo);
 
 if (IntNumberOne > IntNumberTwo) Console.WriteLine("number 1 > number 2");
 else if (IntNumberOne < IntNumberTwo) Console.WriteLine("number 2 > number 1");
-     else Console.WriteLine("number 1 = number 2");
+else Console.WriteLine("number 1 = number 2");
--- lection 1/task 4/Program.cs
+++ lection 1/task 4/Program.cs
@@ -3,6 +3,7 @@
 string numberStr = Console.ReadLine();
 int number = int.Parse(numberStr);
 
-for (int i = 0; i < number; i++){
+for (int i = 0; i < number; i++)
+{
     Console.Write(i + " ");
 }
