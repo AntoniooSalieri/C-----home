8477ddd262909799ce3339c5ff64f690086635aa
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-20 13:43:32 +0200

    lection 1 task 3 complite

---
+++ lection 1/task 3/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("numer: ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+if (number % 2 == 0) Console.WriteLine("can be devided by 2");
+else Console.WriteLine("can not be devided by 2");
