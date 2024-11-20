7b4ab1329a2bb10286065774a3c3553056d0c7bf
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-20 13:48:16 +0200

    lection 1 task 4 complite, all tasks lection 1  done

---
+++ lection 1/task 4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("enter your number");
+
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+for (int i = 0; i < number; i++){
+    Console.Write(i + " ");
+}
