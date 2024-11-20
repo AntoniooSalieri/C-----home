56dbec32e38b54726cf3e4f37b54615d38da56fb
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-20 10:58:44 +0200

    lection 1 task 1 complite

---
+++ lection 1/task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+Console.WriteLine("number 1: ");
+string StrNumberOne =Console.ReadLine();
+
+Console.WriteLine("number 2: ");
+string StrNumberTwo = Console.ReadLine();
+
+int IntNumberOne = int.Parse(StrNumberOne);
+int IntNumberTwo = int.Parse(StrNumberTwo);
+
+if (IntNumberOne > IntNumberTwo) Console.WriteLine("number 1 > number 2");
+else if (IntNumberOne < IntNumberTwo) Console.WriteLine("number 2 > number 1");
+     else Console.WriteLine("number 1 = number 2");
