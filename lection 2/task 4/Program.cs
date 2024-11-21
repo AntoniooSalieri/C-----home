ab3bcf6727491ad4923fcc6ed757ace71c3d6e78
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-21 09:38:00 +0200

    lection 2 task 4 complite

---
+++ lection 2/task 4/Program.cs
@@ -0,0 +1,5 @@
+﻿string dayStr = Console.ReadLine();
+int dayInt = Convert.ToInt32(dayStr);
+if ((dayInt > 0) && (dayInt < 6)) Console.WriteLine("no");
+else if ((dayInt < 8) && (dayInt > 5)) Console.WriteLine("yes");
+    else Console.WriteLine("error");
