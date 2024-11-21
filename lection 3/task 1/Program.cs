78f7fee49db412639e4faef32afac6225af1f3e0
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-21 14:36:13 +0200

    lection 3 task 1 complite

--- lection 3/task 1/Program.cs
+++ lection 3/task 1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string numberStr = Console.ReadLine();
+int numberInt = Convert.ToInt32(numberStr);
+int count = -1;
+string numberRevers = "";
+while (numberInt > 0)
+{
+    numberInt = numberInt / 10;
+    count++;
+}
+
+while (count > -1)
+{
+    numberRevers = numberRevers + numberStr[count];
+    count--;
+}
+
+Console.WriteLine(numberRevers);
+if (numberRevers == numberStr) Console.WriteLine("+");
+else Console.WriteLine("-");
