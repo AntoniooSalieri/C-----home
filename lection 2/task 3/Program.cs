6e75a4614b3e0799772c7e8aa6450644ca6a53f9
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-21 09:40:03 +0200

    lection 2 formating

--- lection 2/task 3/Program.cs
+++ lection 2/task 3/Program.cs
@@ -1,6 +1,7 @@
-﻿string numberStr=Console.ReadLine();
-int numberInt=Convert.ToInt32(numberStr);
-if (numberInt>=100){
+﻿string numberStr = Console.ReadLine();
+int numberInt = Convert.ToInt32(numberStr);
+if (numberInt >= 100)
+{
     Console.WriteLine(numberStr[2]);
 }
 else Console.WriteLine("error");
--- lection 2/task 4/Program.cs
+++ lection 2/task 4/Program.cs
@@ -2,4 +2,4 @@
 int dayInt = Convert.ToInt32(dayStr);
 if ((dayInt > 0) && (dayInt < 6)) Console.WriteLine("no");
 else if ((dayInt < 8) && (dayInt > 5)) Console.WriteLine("yes");
-    else Console.WriteLine("error");
+        else Console.WriteLine("error");
