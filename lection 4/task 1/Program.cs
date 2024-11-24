e6d6d29d1b00efc1be718a02fd3655c1180378f9
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-24 13:12:38 +0200

    lection 4 task 1 complite

---
+++ lection 4/task 1/Program.cs
@@ -0,0 +1,7 @@
+﻿string inputA = Console.ReadLine();
+string inputB = Console.ReadLine();
+
+int numberA = Convert.ToInt32(inputA);
+int numberB = Convert.ToInt32(inputB);
+
+Console.WriteLine(Math.Pow(numberA, numberB));
