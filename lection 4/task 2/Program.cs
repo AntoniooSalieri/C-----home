c51f4a235086b47581da6343ebca04dfdf66bd71
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-24 13:24:30 +0200

    lection 4 task 2

---
+++ lection 4/task 2/Program.cs
@@ -0,0 +1,11 @@
+﻿int IntInput (){
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int summ = 0;
+int number = IntInput();
+for (int i = number; i > 0; i = i / 10){
+    summ = summ + i%10;
+}
+
+Console.WriteLine(summ);
