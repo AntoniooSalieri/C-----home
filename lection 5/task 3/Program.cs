9f2755a7adc185ffe96c7531fdbeb48a9c442dc0
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-25 10:40:18 +0200

    lection 5 tasl 3 complite

---
+++ lection 5/task 3/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] arr, int length){
+    for(int i = 0; i < length; i ++){
+        arr[i] = new Random().Next(0, 10);
+    }
+}
+void Printer(int[] arr, int length){
+    for(int i = 0; i < length; i++){
+        Console.Write($" {arr[i]} ");
+    }
+}
+int Difference(int[] arr, int length){
+    int min = arr[0];
+    int max = arr[0];
+    for(int i = 1; i < length; i++){
+        if(max<arr[i]) max = arr[i];
+        else if (min > arr[i]) min = arr[i];
+    }
+    int dif = max - min;
+    return dif;
+}
+
+
+int arrayLength = 10;
+int[] array = new int[arrayLength];
+FillArray(array, arrayLength);
+Printer(array, arrayLength);
+Console.WriteLine();
+Console.WriteLine(Difference(array, arrayLength));
