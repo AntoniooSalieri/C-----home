6dd7b40a4db549df85f6bc706c98675e04185b66
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-25 10:13:17 +0200

    lection 5 tasl 1 complite

---
+++ lection 5/task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] arr, int arrayLength){
+    for (int i = 0; i < arrayLength; i++){
+        arr[i] = new Random().Next(100, 1000);
+    }
+}
+int Counter(int[] arr, int arrayLength){
+    int count = 0;
+    for (int i = 0; i < arrayLength; i++){
+        if (arr[i]%2 == 0){
+            count++;
+        }
+    }
+    return count;
+}
+void Printer(int[] arr, int arrayLength){
+    for (int i = 0; i < arrayLength; i++){
+        Console.Write($" {arr[i]} ");
+    }
+}
+
+int length = 10;
+int[] array = new int[length];
+
+FillArray(array, length);
+Printer(array, length);
+Console.WriteLine();
+Console.WriteLine(Counter(array, length));
