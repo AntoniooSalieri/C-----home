42a1adab6beb030ace0031bc86e483f5853e890d
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-27 11:03:00 +0200

    lection 5 task 3 fixed

--- lection 5/task 3/Program.cs
+++ lection 5/task 3/Program.cs
@@ -1,27 +1,27 @@
-﻿void FillArray(int[] arr, int length){
+﻿void FillArray(double[] arr, int length){
     for(int i = 0; i < length; i ++){
-        arr[i] = new Random().Next(0, 10);
+        arr[i] = new Random().Next(0, 10)+Math.Round(new Random().NextDouble(), 2);
     }
 }
-void Printer(int[] arr, int length){
+void Printer(double[] arr, int length){
     for(int i = 0; i < length; i++){
         Console.Write($" {arr[i]} ");
     }
 }
-int Difference(int[] arr, int length){
-    int min = arr[0];
-    int max = arr[0];
+double Difference(double[] arr, int length){
+    double min = arr[0];
+    double max = arr[0];
     for(int i = 1; i < length; i++){
         if(max<arr[i]) max = arr[i];
         else if (min > arr[i]) min = arr[i];
     }
-    int dif = max - min;
+    double dif = max - min;
     return dif;
 }
 
 
 int arrayLength = 10;
-int[] array = new int[arrayLength];
+double[] array = new double[arrayLength];
 FillArray(array, arrayLength);
 Printer(array, arrayLength);
 Console.WriteLine();
