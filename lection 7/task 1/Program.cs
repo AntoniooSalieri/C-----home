4cb28cd580b56f802b837e83dac08be5010d8871
IonaLn <antonio.fon.salieri@gmail.com>
2024-12-11 18:23:43 +0200

    lection 7 tasks 1, 2 complite

---
+++ lection 7/task 1/Program.cs
@@ -0,0 +1,32 @@
+﻿int m = 5,
+    n = 5;
+
+int[,] array = new int[m, n];
+
+
+void FillArray(int[,] arr, int row = 0, int column = 0){
+    if (column < arr.GetLength(1)){
+        arr[row, column] = 1;
+        FillArray(arr, row, column + 1);
+    }
+    else{
+        if (row + 1 < arr.GetLength(0)){      //почему -1?
+            FillArray(arr, row + 1, 0);
+        }
+    }
+}
+
+
+void PrintArray(int[,] arr){
+    for (int i = 0; i < arr.GetLength(0); i++){
+        for (int j = 0; j < arr.GetLength(1); j++){
+            Console.Write(arr[i, j]);
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+
+FillArray(array);
+PrintArray(array);
---
+++ lection 7/task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+bool Finder(int[,] array, int m, int n){
+    m --;
+    n --;
+    if (n < array.GetLength(0)){
+        if (m < array.GetLength(1)){
+            Console.WriteLine( array[n, m]);
+            return true;
+        }
+        else return false;
+    }
+    else return false;
+}
+
+int[,] arrayForTest = {
+        {0, 0, 0},
+        {0, 0, 0}
+};
+int mTest = 3;
+int nTest = 2;
+
+Console.WriteLine(Finder(arrayForTest, mTest, nTest));
