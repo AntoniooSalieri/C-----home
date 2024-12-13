2a5a4cd5e2ea5b4003f4c0fc19ab4e0d111a2bfa
IonaLn <antonio.fon.salieri@gmail.com>
2024-12-13 12:48:51 +0200

    lection 8 task 1 complite

--- lection 8/task 1/Program.cs
+++ lection 8/task 1/Program.cs
@@ -4,30 +4,38 @@ Random random = new Random();
 void FillArrayRec (int[,] arr, int m = 0, int n = 0)
 {
     if (m < arr.GetLength(0)){
-        arr[n, m] = m;// random.Next(0, 10);
+        arr[n, m] = random.Next(0, 10);
         FillArrayRec(arr, m+1, n);
     }
     else{
         if (n + 1 < arr.GetLength(1)){
-            Console.WriteLine(n);
             FillArrayRec(arr, 0, n+1);
         }
     }
 }
 
-void SortOneLine (int[,] arr, int line, int position = 0);
+void SortOneLineRec (int[,] arr, int line, int position = 0)
 {
     int max = arr[line, position];
-    for (int i = position; i < arr.Length; i++){
+    int positionMaxElement = position;
+    for (int i = position; i < arr.GetLength(1); i++){
         if (max < arr[line, i]) {
             max = arr[line, i];
-            int positionMaxElement = i;
+            positionMaxElement = i;
         }
     }
     arr[line, positionMaxElement] = arr[line, position];
     arr[line, position] = max;
+    if (position < arr.GetLength(1)-1){
+        SortOneLineRec(arr, line, position + 1);
+    }
 }
 
+void ArraySorter (int[,] arr){
+    for (int i = 0; i < arr.GetLength(0); i++){
+        SortOneLineRec ( arr, i );
+    }
+}
 
 void PrintArray(int[,] arr)
 {
@@ -43,3 +51,5 @@ FillArrayRec(testArray);
 Console.WriteLine();
 PrintArray(testArray);
 Console.WriteLine();
+ArraySorter(testArray);
+PrintArray(testArray);
